2ddb0999548ad7900c63e9de40a947023e5179f7
Aleksandr.L <xvxxxy@yandex.ru>
2021-09-10 22:03:27 +0300

    filtration 5.2

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -3,18 +3,28 @@ import User from "./user";
 import { paginate } from "../utils/paginate";
 import Pagination from "./pagination";
 import PropTypes from "prop-types";
+import GroupList from "./groupList";
+import api from "../api";
 
 function Users({ users: allUsers, onDelete }) {
     const count = allUsers.length;
     const pageSize = 4;
     const [currentPage, setcurrentPage] = useState(1);
+    const [professions] = useState(api.professions.fetchAll());
     if (count === 0) return null;
     const handlePageChange = (pageIndex) => {
         setcurrentPage(pageIndex);
     };
     const users = paginate(allUsers, currentPage, pageSize);
+    const handleProfessionsSelect = (params) => {
+        console.log(params);
+    };
     return (
         <>
+            <GroupList
+                items={professions}
+                onItemSelect={handleProfessionsSelect}
+            />
             <table className="table">
                 <thead>
                     <tr>
