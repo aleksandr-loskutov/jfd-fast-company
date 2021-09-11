be688df3ddc2bd9d1ca9049a462a48d201c7be6e
Aleksandr.L <xvxxxy@yandex.ru>
2021-09-11 17:17:32 +0300

    filtration #KP1

--- src/components/groupList.jsx
+++ src/components/groupList.jsx
@@ -1,16 +1,42 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function GroupList() {
+function GroupList({
+    items,
+    valueProperty,
+    contentProperty,
+    onItemSelect,
+    selectedItem
+}) {
     return (
         <ul className="list-group">
-            <li className="list-group-item active" aria-current="true">
-                An active item
-            </li>
-            <li className="list-group-item">A second item</li>
-            <li className="list-group-item">A third item</li>
-            <li className="list-group-item">A fourth item</li>
-            <li className="list-group-item">And a fifth one</li>
+            {(Array.isArray(items) ? items : Object.values(items)).map(
+                (item) => (
+                    <li
+                        key={item[valueProperty]}
+                        className={
+                            "list-group-item" +
+                            (item === selectedItem ? " active" : "")
+                        }
+                        onClick={() => onItemSelect(item)}
+                        role={"button"}
+                    >
+                        {item[contentProperty]}
+                    </li>
+                )
+            )}
         </ul>
     );
 }
+GroupList.defaultProps = {
+    valueProperty: "_id",
+    contentProperty: "name"
+};
+GroupList.propTypes = {
+    items: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
+    valueProperty: PropTypes.string.isRequired,
+    contentProperty: PropTypes.string.isRequired,
+    onItemSelect: PropTypes.func,
+    selectedItem: PropTypes.object
+};
 export default GroupList;
--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,62 +1,99 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import User from "./user";
 import { paginate } from "../utils/paginate";
 import Pagination from "./pagination";
 import PropTypes from "prop-types";
 import GroupList from "./groupList";
 import api from "../api";
+import SearchStatus from "./searchStatus";
 
 function Users({ users: allUsers, onDelete }) {
-    const count = allUsers.length;
+    const [currentPage, setCurrentPage] = useState(1);
+    const [professions, setProfessions] = useState();
+    const [selectedProf, setSelectedProf] = useState();
+
     const pageSize = 4;
-    const [currentPage, setcurrentPage] = useState(1);
-    const [professions] = useState(api.professions.fetchAll());
-    if (count === 0) return null;
+    useEffect(() => {
+        api.professions.fetchAll().then((data) => {
+            setProfessions(data);
+        });
+    }, []);
+    useEffect(() => {
+        setCurrentPage(1);
+    }, [selectedProf]);
     const handlePageChange = (pageIndex) => {
-        setcurrentPage(pageIndex);
+        setCurrentPage(pageIndex);
     };
-    const users = paginate(allUsers, currentPage, pageSize);
-    const handleProfessionsSelect = (params) => {
-        console.log(params);
+    const handleProfessionsSelect = (item) => {
+        setSelectedProf(item);
     };
+    const filteredUsers = selectedProf
+        ? allUsers.filter((user) => user.profession.name === selectedProf.name)
+        : allUsers;
+    console.log("filteredUsers", filteredUsers);
+    const count = filteredUsers.length;
+    const usersCrop = paginate(filteredUsers, currentPage, pageSize);
+    const clearFilter = () => {
+        setSelectedProf();
+    };
+
     return (
-        <>
-            <GroupList
-                items={professions}
-                onItemSelect={handleProfessionsSelect}
-            />
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th scope="col">Имя</th>
-                        <th scope="col">Качества</th>
-                        <th scope="col">Профессия</th>
-                        <th scope="col">Встретился раз</th>
-                        <th scope="col">Оценка</th>
-                        <th scope="col">Избранное</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {users.map((user) => {
-                        return (
-                            <User
-                                key={user._id}
-                                user={user}
-                                onDelete={onDelete}
-                            />
-                        );
-                    })}
-                </tbody>
-            </table>
-            <Pagination
-                itemsCount={count}
-                pageSize={pageSize}
-                currentPage={currentPage}
-                onPageChange={handlePageChange}
-            />
-        </>
+        <div className="d-flex">
+            {professions && (
+                <div className="d-flex flex-column flex-shrink-0 p-3">
+                    <GroupList
+                        selectedItem={selectedProf}
+                        items={professions}
+                        onItemSelect={handleProfessionsSelect}
+                    />
+                    <button
+                        className={"btn btn-secondary mt-2"}
+                        onClick={clearFilter}
+                    >
+                        Очистить
+                    </button>
+                </div>
+            )}
+            <div className="d-flex flex-column">
+                <SearchStatus usersTotal={count} />
+                {count > 0 && (
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">Имя</th>
+                                <th scope="col">Качества</th>
+                                <th scope="col">Профессия</th>
+                                <th scope="col">Встретился раз</th>
+                                <th scope="col">Оценка</th>
+                                <th scope="col">Избранное</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {usersCrop.map((user) => {
+                                return (
+                                    <User
+                                        key={user._id}
+                                        user={user}
+                                        onDelete={onDelete}
+                                    />
+                                );
+                            })}
+                        </tbody>
+                    </table>
+                )}
+                <div className="d-flex justify-content-center">
+                    <Pagination
+                        itemsCount={count}
+                        pageSize={pageSize}
+                        currentPage={currentPage}
+                        onPageChange={handlePageChange}
+                    />
+                </div>
+            </div>
+        </div>
     );
 }
+
 Users.propTypes = {
     users: PropTypes.array.isRequired,
     onDelete: PropTypes.func.isRequired
