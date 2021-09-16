87af39feb9f14f553543e12b9675b6124d476b89
Aleksandr.L <xvxxxy@yandex.ru>
2021-09-16 19:01:33 +0300

    Sorting KP 7.6

--- src/components/usersTable.jsx
+++ src/components/usersTable.jsx
@@ -1,28 +1,25 @@
 import React from "react";
-import User from "./user";
+// import User from "./user";
 import PropTypes from "prop-types";
 import TableHeader from "./tableHeader";
+import TableBody from "./tableBody";
 function UserTable({ users, onSort, selectedSort, ...rest }) {
     const columns = {
-        name: { iter: "name", name: "Имя" },
+        name: { path: "name", name: "Имя" },
         qualities: { name: "Качества" },
-        professions: { iter: "profession.name", name: "Профессия" },
+        professions: { path: "profession.name", name: "Профессия" },
         completedMeetings: {
-            iter: "completedMeetings",
+            path: "completedMeetings",
             name: "Встретился, раз"
         },
-        rate: { iter: "rate", name: "Оценка" },
-        bookmark: { iter: "bookmark", name: "Избранное" },
+        rate: { path: "rate", name: "Оценка" },
+        bookmark: { path: "bookmark", name: "Избранное" },
         delete: {}
     };
     return (
         <table className="table">
             <TableHeader {...{ selectedSort, onSort, columns }} />
-            <tbody>
-                {users.map((user) => {
-                    return <User key={user._id} user={user} {...rest} />;
-                })}
-            </tbody>
+            <TableBody {...{ columns, data: users }} />
         </table>
     );
 }
