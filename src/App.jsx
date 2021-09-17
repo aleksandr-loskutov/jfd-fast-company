489cf6abe6c0be2039b37d65753304276bf85590
Aleksandr.L <aleksandranger@gmail.com>
2021-09-17 17:18:08 +0300

    Sorting KP 7.10

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,6 @@
-import React, { useEffect, useState } from "react";
-import api from "./api";
+import React from "react";
 import Users from "./components/users.jsx";
 function App() {
-    const [users, setUsers] = useState([]);
-
-    useEffect(() => {
-        api.users.fetchAll().then((data) => {
-            setUsers(data);
-        });
-    }, []);
-    const handleToggleBookMark = (id) => {
-        setUsers(
-            users.map((user) => {
-                if (user._id === id) {
-                    return { ...user, bookmark: !user.bookmark };
-                }
-                return user;
-            })
-        );
-        console.log(id);
-    };
-    const handleDelete = (userId) => {
-        setUsers(
-            users.reduce((newArr, user) => {
-                if (user._id !== userId) {
-                    newArr.push(user);
-                }
-                return newArr;
-            }, [])
-        );
-    };
-    return (
-        <>
-            <Users
-                users={users}
-                onDelete={handleDelete}
-                onToggleBookMark={handleToggleBookMark}
-            />
-        </>
-    );
+    return <Users />;
 }
 export default App;
---
+++ src/components/table.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import PropTypes from "prop-types";
+import TableBody from "./tableBody";
+import TableHeader from "./tableHeader";
+function Table({ selectedSort, onSort, columns, data, children }) {
+    return (
+        <table className="table">
+            {children || (
+                <>
+                    <TableHeader {...{ selectedSort, onSort, columns }} />
+                    <TableBody {...{ columns, data }} />
+                </>
+            )}
+        </table>
+    );
+}
+Table.propTypes = {
+    selectedSort: PropTypes.object,
+    onSort: PropTypes.func,
+    columns: PropTypes.object,
+    data: PropTypes.object.array,
+    children: PropTypes.array
+};
+export default Table;
