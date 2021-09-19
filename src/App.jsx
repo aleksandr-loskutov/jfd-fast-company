cca5152ba6614bfcc545d03fd61f0302f3db3bce
Aleksandr.L <aleksandranger@gmail.com>
2021-09-19 15:58:56 +0300

    added main,login,users,navBar + routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
 import React from "react";
 import Users from "./components/users.jsx";
+import NavBar from "./components/navBar";
+import Main from "./components/main";
+import { Route, Switch } from "react-router-dom";
+import Login from "./components/login";
 function App() {
-    return <Users />;
+    return (
+        <>
+            <NavBar />
+            <Switch>
+                <Route path="/login" component={Login} />
+                <Route path="/users/:userId?" component={Users} />
+                <Route path="/" exact component={Main} />
+            </Switch>
+        </>
+    );
 }
 export default App;
