b188c49d7c2b72e25f5c48abd3714a79bf977011
Aleksandr.L <aleksandranger@gmail.com>
2021-09-19 22:35:13 +0300

    8 KP#1 searchStatus display bug fix

--- src/components/searchStatus.jsx
+++ src/components/searchStatus.jsx
@@ -2,39 +2,40 @@ import React, { useRef } from "react";
 import PropTypes from "prop-types";
 
 function SearchStatus({ usersTotal }) {
-    const firstInit = useRef(true);
-    const declOfNum = (usersTotal, titles) => {
+    const loaded = useRef(false);
+    let str = usersTotal.length > 0 ? `${usersTotal.length} ` : "";
+    const declOfNum = (num, titles) => {
         const cases = [0, 2, 1, 1, 1, 2];
         return titles[
-            usersTotal % 100 > 4 && usersTotal % 100 < 20
+            num % 100 > 4 && num % 100 < 20
                 ? 2
-                : cases[usersTotal % 10 < 5 ? usersTotal % 10 : 5]
+                : cases[num % 10 < 5 ? num % 10 : 5]
         ];
     };
-    let str = usersTotal > 0 ? `${usersTotal} ` : "";
-    str += declOfNum(usersTotal, [
+    const setLoaded = () => {
+        loaded.current = true;
+        return "Загрузка...";
+    };
+    str += declOfNum(usersTotal.length, [
         "Никто с тобой не тусанет",
         "человека тусанут с тобой сегодня",
         "человек тусанёт с тобой сегодня"
     ]);
-
-    if (firstInit.current) {
-        str = "Загрузка...";
-        firstInit.current = false;
-    }
+    console.log("loaded.current", loaded.current);
     return (
         <h4>
             <span
                 className={
-                    "badge m-2 bg-" + (usersTotal > 0 ? "primary" : "danger")
+                    "badge m-2 bg-" +
+                    (usersTotal.length > 0 ? "primary" : "danger")
                 }
             >
-                {str}
+                {!loaded.current && usersTotal.length === 0 ? setLoaded() : str}
             </span>
         </h4>
     );
 }
 SearchStatus.propTypes = {
-    usersTotal: PropTypes.number.isRequired
+    usersTotal: PropTypes.array.isRequired
 };
 export default SearchStatus;
