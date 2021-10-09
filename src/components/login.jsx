03a73e3da5654e9991176f5ea4c0241843327925
Aleksandr.L <xvxxxy@yandex.ru>
2021-10-09 12:46:37 +0300

    login form draft

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Login = () => {
-    return <h2>Login</h2>;
-};
+function Login(props) {
+    const [data, setData] = useState({ email: "", password: "" });
+    const handleChange = ({ target }) => {
+        setData((prevState) => ({
+            ...prevState,
+            [target.name]: target.value
+        }));
+    };
+
+    return (
+        <form action="">
+            <div>
+                <label htmlFor="data">Email</label>
+                <input
+                    type="text"
+                    id="email"
+                    value={data.email}
+                    onChange={handleChange}
+                    name="email"
+                />
+            </div>
+            <div>
+                <label htmlFor="password">password</label>
+                <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    value={data.password}
+                    onChange={handleChange}
+                />
+            </div>
+        </form>
+    );
+}
 
 export default Login;
