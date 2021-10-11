7e649ca3eefeb4cb9d3927a4007696d47d41401b
Aleksandr.L <xvxxxy@yandex.ru>
2021-10-11 23:48:35 +0300

    form and textField component

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,16 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TextField from "./textField";
 
 function Login(props) {
     const [data, setData] = useState({ email: "", password: "" });
+    const [errors, setErrors] = useState({});
     const handleChange = ({ target }) => {
         setData((prevState) => ({
             ...prevState,
             [target.name]: target.value
         }));
     };
+    const validate = () => {
+        const errors = {};
+        for (const fieldName in data) {
+            if (data[fieldName].trim() === "") {
+                errors[fieldName] = `${fieldName} обязательно для заполнения`;
+            }
+        }
+        setErrors(errors);
+        return Object.keys(errors).length === 0;
+    };
+    useEffect(() => {
+        validate();
+    }, [data]);
     const handleSubmit = (event) => {
         event.preventDefault();
+        const isValid = validate();
+        if (!isValid) return;
         console.log(data);
     };
     return (
@@ -20,6 +36,7 @@ function Login(props) {
                 name="email"
                 value={data.email}
                 onChange={handleChange}
+                error={errors.email}
             />
             <TextField
                 label="Пароль"
@@ -27,6 +44,7 @@ function Login(props) {
                 name="password"
                 value={data.password}
                 onChange={handleChange}
+                error={errors.password}
             />
             <button>Отправить</button>
         </form>
--- src/components/textField.jsx
+++ src/components/textField.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-function TextField({ label, type, name, value, onChange }) {
+function TextField({ label, type, name, value, onChange, error }) {
     return (
         <div>
             <label htmlFor={name}>{label}</label>
@@ -11,6 +11,7 @@ function TextField({ label, type, name, value, onChange }) {
                 value={value}
                 onChange={onChange}
             />
+            {error && <p>{error}</p>}
         </div>
     );
 }
@@ -22,6 +23,7 @@ TextField.propTypes = {
     type: PropTypes.string,
     name: PropTypes.string,
     value: PropTypes.string,
-    onChange: PropTypes.func
+    onChange: PropTypes.func,
+    error: PropTypes.string
 };
 export default TextField;
