80f3d8f50b8dbb6740829875ee1f82691accf393
Aleksandr.L <xvxxxy@yandex.ru>
2021-10-13 21:57:01 +0300

    added styles and improved validation

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -1,12 +1,36 @@
 export function validator(data, config) {
     const errors = {};
     function validate(validateMethod, data, config) {
+        let statusValidate;
         switch (validateMethod) {
             case "isRequired":
-                if (data.trim() === "") return config.message;
+                statusValidate = data.trim() === "";
+                break;
+            case "isEmail": {
+                const emailRegExp = /^\S+@\S+\.\S+$/g;
+                statusValidate = !emailRegExp.test(data);
+                break;
+            }
+            case "isCapitalSymbol": {
+                const capitalRegExp = /[A-Z]+/g;
+                statusValidate = !capitalRegExp.test(data);
+                break;
+            }
+            case "isContainDigit": {
+                const digitRegExp = /\d+/g;
+                statusValidate = !digitRegExp.test(data);
+                break;
+            }
+            case "min": {
+                statusValidate = data.length < config.value;
+                break;
+            }
+            default:
                 break;
         }
+        if (statusValidate) return config.message;
     }
+
     for (const fieldName in data) {
         for (const validateMethod in config[fieldName]) {
             const error = validate(
@@ -14,8 +38,8 @@ export function validator(data, config) {
                 data[fieldName],
                 config[fieldName][validateMethod]
             );
-            if (error) {
-                errors[fieldName] = errors;
+            if (error && !errors[fieldName]) {
+                errors[fieldName] = error;
             }
         }
     }
