73daa1508c6251f0eeda56d2ca13a004c42e1c31
Aleksandr.L <xvxxxy@yandex.ru>
2021-10-13 18:24:57 +0300

    added validator

---
+++ src/utils/validator.js
@@ -0,0 +1,23 @@
+export function validator(data, config) {
+    const errors = {};
+    function validate(validateMethod, data, config) {
+        switch (validateMethod) {
+            case "isRequired":
+                if (data.trim() === "") return config.message;
+                break;
+        }
+    }
+    for (const fieldName in data) {
+        for (const validateMethod in config[fieldName]) {
+            const error = validate(
+                validateMethod,
+                data[fieldName],
+                config[fieldName][validateMethod]
+            );
+            if (error) {
+                errors[fieldName] = errors;
+            }
+        }
+    }
+    return errors;
+}
