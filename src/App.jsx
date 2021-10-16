79b7ff89ab2c8c6ba17d1813433eb433380de4da
Aleksandr.L <xvxxxy@yandex.ru>
2021-10-16 19:05:44 +0300

    Added button component & new route for edit user data

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import UsersListPage from "./components/page/userListPage/usersListPage.jsx";
+import UserPage from "./components/page/userPage";
 import NavBar from "./components/ui/navBar";
 import Main from "./layouts/main";
 import { Route, Switch } from "react-router-dom";
@@ -10,6 +11,7 @@ function App() {
             <NavBar />
             <Switch>
                 <Route path="/login/:type?" component={Login} />
+                <Route path="/users/:userId/edit" component={UserPage} />
                 <Route path="/users/:userId?" component={UsersListPage} />
                 <Route path="/" exact component={Main} />
             </Switch>
