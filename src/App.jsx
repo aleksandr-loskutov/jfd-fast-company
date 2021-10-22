a44a545023ec895f5be9e933c55706652af0b035
Aleksandr.L <xvxxxy@yandex.ru>
2021-10-22 20:46:36 +0300

    App.jsx refactor for layouts

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,18 @@ import UsersListPage from "./components/page/userListPage/usersListPage.jsx";
 import UserPage from "./components/page/userPage";
 import NavBar from "./components/ui/navBar";
 import Main from "./layouts/main";
-import { Route, Switch } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 import Login from "./layouts/login";
+import Users from "./layouts/users";
 function App() {
     return (
         <>
             <NavBar />
             <Switch>
+                <Route path="/users/:userId?/:edit?" component={Users} />
                 <Route path="/login/:type?" component={Login} />
-                <Route path="/users/:userId/edit" component={UserPage} />
-                <Route path="/users/:userId?" component={UsersListPage} />
                 <Route path="/" exact component={Main} />
+                <Redirect to="/" />
             </Switch>
         </>
     );
