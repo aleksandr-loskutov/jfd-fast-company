17e738c8878230c42eea7327c6456aac5ce5ec06
Aleksandr.L <xvxxxy@yandex.ru>
2021-10-23 21:47:40 +0300

    users layout refactor

--- src/layouts/users.jsx
+++ src/layouts/users.jsx
@@ -2,10 +2,23 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import UserPage from "../components/page/userPage";
 import UsersListPage from "../components/page/userListPage";
+import UserEditPage from "../components/page/userPage/userEditPage";
 function Users() {
     const params = useParams();
-    const { userId } = params;
-    return <>{userId ? <UserPage userId={userId} /> : <UsersListPage />}</>;
+    const { userId, edit } = params;
+    return (
+        <>
+            {userId ? (
+                edit ? (
+                    <UserEditPage />
+                ) : (
+                    <UserPage userId={userId} />
+                )
+            ) : (
+                <UsersListPage />
+            )}
+        </>
+    );
 }
 
 export default Users;
