bfb810ed95c3f7ae64290b3f7421e34d8e840810
Aleksandr.L <xvxxxy@yandex.ru>
2021-11-09 11:18:28 +0300

    reset project to course version

--- src/utils/misc.js
+++
@@ -1,9 +0,0 @@
-function convertForSelect(data) {
-    return !Array.isArray(data) && typeof data === "object"
-        ? Object.keys(data).map((optionName) => ({
-              label: data[optionName].name,
-              value: data[optionName]._id
-          }))
-        : data;
-}
-export default convertForSelect;
