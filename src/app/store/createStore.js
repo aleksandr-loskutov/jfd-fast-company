d4df1e514fa45aa9e86b0546e585c6feff5e97e0
Aleksandr.L <xvxxxy@yandex.ru>
2022-02-04 22:44:41 +0300

    Added professions & users

--- src/app/store/createStore.js
+++ src/app/store/createStore.js
@@ -1,8 +1,14 @@
 import qualitiesReducer from "./qualities";
+import professionsReducer from "./professions";
+import usersReducer from "./users";
 
 const { combineReducers, configureStore } = require("@reduxjs/toolkit");
 
-const rootReducer = combineReducers({ qualities: qualitiesReducer });
+const rootReducer = combineReducers({
+    qualities: qualitiesReducer,
+    professions: professionsReducer,
+    users: usersReducer
+});
 
 export function createStore() {
     return configureStore({
